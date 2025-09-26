4ce0796691b40c1b3ee61d576c691b79ef19d438
Yuri-cmd <yurim011218@gmail.com>
2025-09-26 18:24:43 -0500

    fix: Update minSdkVersion in build.gradle.kts and increment app version in pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.arequipago.arequipagocreditos"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
